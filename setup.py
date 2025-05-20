fd08a49bde5bfff3cc8f313629b0b0fed9c365bf
Bhagii2508 <bhagii2508@gmail.com>
2025-05-20 00:57:15 -0400

    Updated Setup.py to recognize local dir as packages for easy imports

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Bhagii",
+    author_email="bhagii2508@gmail.com",
+    packages=find_packages()
+)
